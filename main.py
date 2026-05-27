1d74dd284e011055b14d40ef6b7a4276fdfa5492
NolanHoff <nolanhoff14@gmail.com>
2026-05-27 16:36:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@
 #Interpreter: MicroPython
 #Main Code
 #main.py
-#Last Edit: NolanRH on 5/21/2026
+#Last Edit: NolanRH on 5/27/2026
 
 #Credit
 #If Labelled 'Debug Execution Timer' then it was copied from 'How to measure EXECUTION TIME in Python ⌚' by 'Bro Code' on YouTube
 
 #Project .py Imports
-from hdc302x_sensor import HDC302X_Sensors
+from hdc302x_sensors import HDC302X_Sensors
 
 #main.py Imports
 import utime, time
@@ -20,13 +20,15 @@ import utime, time
 while True:
     start_timer = time.ticks_us() #Debug Execution Timer
     
-    '''----------------------------------------------------------------------------------------------------------------------'''
+    #----------------------------------------------------------------------------------------------------------------------
     #Debugging Functions
     HDC302X_Sensors.debug_readings() #Calls a hdc302x_sensor.py Debug Function
-    '''----------------------------------------------------------------------------------------------------------------------'''
+    #----------------------------------------------------------------------------------------------------------------------
     
     end_timer = time.ticks_us() #Debug Execution Timer
     elapsed_time = end_timer - start_timer #Debug Execution Timer
+    
+    print(f"\n================================MAIN LOOP LOG================================")
     print(f"Execution Time: {elapsed_time/1000000:.5f} seconds") #Debug Execution Timer
     print('Main Loop Executed Successfully!\n') #Debug Print
     
