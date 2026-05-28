1c2d0e963af2b71b2c7e69db87d6cf26576caa50
NolanHoff <nolanhoff14@gmail.com>
2026-05-28 20:43:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 #Micro Controller: Raspberry Pi Pico W
 #Interpreter: MicroPython
 #Main Code: main.py
-#Last Edit: NolanRH on 5/27/2026
+#Last Edit: NolanRH on 5/28/2026
 
 #Project .py Imports
 from hdc302x_sensors import HDC302X_Sensors
@@ -12,13 +12,23 @@ from hdc302x_sensors import HDC302X_Sensors
 #main.py Imports
 import utime, time
 
+#Toggles 1=True | 0=False
+debugToggle = 1
+
 #Main Loop
 while True:
     start_timer = time.ticks_us() #Debug Execution Timer
     
     #----------------------------------------------------------------------------------------------------------------------
+    #Main Functions
+    #PUT MAIN FUNCTIONS HERE
+    
+    #----------------------------------------------------------------------------------------------------------------------
     #Debugging Functions
-    HDC302X_Sensors.debug_readings() #Calls a hdc302x_sensor.py Debug Function
+    if debugToggle == 1:
+        HDC302X_Sensors.debug_readings() #Calls a hdc302x_sensor.py Debug Sensor Logger Print
+    else:
+        continue
     #----------------------------------------------------------------------------------------------------------------------
     
     end_timer = time.ticks_us() #Debug Execution Timer
