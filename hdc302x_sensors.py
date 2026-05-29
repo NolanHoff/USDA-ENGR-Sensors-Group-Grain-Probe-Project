9200dbd630a467033d748e4d64ec9d61b5bc202e
NolanHoff <nolanhoff14@gmail.com>
2026-05-29 16:44:23 +0000

    Reduced & Refactored hdc302x_sensors.py
    i2c0 & i2c1 are assigned to individual sensors, so bus0 & bus1 were combined into sensorList and code was refactored to match, thus removing the redundancy from a duplicate loop for each bus

--- hdc302x_sensors.py
+++ hdc302x_sensors.py
@@ -8,7 +8,7 @@
 
 #hdc302x_sensor.py Imports
 from machine import Pin, I2C
-import time
+import utime, time
 import hdc302x_library #Library Import (hdc302x_library.py)
 
 #Class Allows Import of all Included Functions
@@ -17,16 +17,15 @@ class HDC302X_Sensors:
     i2c0 = I2C(0, scl=Pin(1), sda=Pin(0), freq=100000) #I2C Bus 0 (GP1=SCL, GP0=SDA)
     i2c1 = I2C(1, scl=Pin(3), sda=Pin(2), freq=100000) #I2C Bus 1 (GP3=SCL, GP2=SDA)
     
-    bus0 = [
-        ("Sensor 1", "x044", hdc302x_library.HDC302X(i2c0, address=0x44)), #Sensor 1
-        ("Sensor 2", "x045", hdc302x_library.HDC302X(i2c0, address=0x45)), #Sensor 2
-        ("Sensor 3", "x046", hdc302x_library.HDC302X(i2c0, address=0x46)), #Sensor 3
-        ("Sensor 4", "x047", hdc302x_library.HDC302X(i2c0, address=0x47))] #Sensor 4
-    bus1 = [
-        ("Sensor 5", "x044", hdc302x_library.HDC302X(i2c1, address=0x44)), #Sensor 5
-        ("Sensor 6", "x045", hdc302x_library.HDC302X(i2c1, address=0x45)), #Sensor 6
-        ("Sensor 7", "x046", hdc302x_library.HDC302X(i2c1, address=0x46)), #Sensor 7
-        ("Sensor 8", "x047", hdc302x_library.HDC302X(i2c1, address=0x47))] #Sensor 8
+    sensorList = [
+        ("Sensor 1", "x044", hdc302x_library.HDC302X(i2c0, address=0x44)), #Sensor 1 on Bus 0
+        ("Sensor 2", "x045", hdc302x_library.HDC302X(i2c0, address=0x45)), #Sensor 2 on Bus 0
+        ("Sensor 3", "x046", hdc302x_library.HDC302X(i2c0, address=0x46)), #Sensor 3 on Bus 0
+        ("Sensor 4", "x047", hdc302x_library.HDC302X(i2c0, address=0x47)), #Sensor 4 on Bus 0
+        ("Sensor 5", "x044", hdc302x_library.HDC302X(i2c1, address=0x44)), #Sensor 5 on Bus 1
+        ("Sensor 6", "x045", hdc302x_library.HDC302X(i2c1, address=0x45)), #Sensor 6 on Bus 1
+        ("Sensor 7", "x046", hdc302x_library.HDC302X(i2c1, address=0x46)), #Sensor 7 on Bus 1
+        ("Sensor 8", "x047", hdc302x_library.HDC302X(i2c1, address=0x47))] #Sensor 8 on Bus 1
     
     
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~DEBUGGING FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -36,82 +35,50 @@ class HDC302X_Sensors:
     
     @staticmethod 
     def debug_readings():
+    #Debug Data for sensorList Print
         timestamps = [] #Lists all I2C Sensors Timestamps for Print Statements
-    #Debug Data for Bus 0 Print
-        sensorsBus0 = [] #Lists Bus 0 Sensors Names for Print Statements
-        addrsBus0 = [] #Lists Bus 0 Sensors Addresses for Print Statements
-        tempsBus0 = [] #Lists Bus 0 Sensors Temperatures for Print Statements
-        humiditiesBus0 = [] #Lists Bus 0 Humidities Names for Print Statements
-        for i, (sensor_name, addrs, sensor) in enumerate(HDC302X_Sensors.bus0):
+        nameList = [] #Lists Sensor Names for Print Statements
+        addrsList = [] #Lists Sensor Addresses for Print Statements
+        tempsList = [] #Lists Sensor Temperatures for Print Statements
+        humsList = [] #Lists Bus 0 Humidities Names for Print Statements
+        for i, (sensor_name, addrs, sensor) in enumerate(HDC302X_Sensors.sensorList):
             try:
-                #Grabs Bus 0 Sensor Data
+                #Grabs Sensor Data
                 temp = sensor.temperature
                 hum = sensor.relative_humidity
-                #Appends Bus 0 Sensor Data
-                sensorsBus0.append(sensor_name)
-                addrsBus0.append(addrs)
-                tempsBus0.append(temp)
-                humiditiesBus0.append(hum)
+                #Appends Sensor Data
+                nameList.append(sensor_name)
+                addrsList.append(addrs)
+                tempsList.append(temp)
+                humsList.append(hum)
                 #Takes Current Timestamp
                 now = time.localtime() #To Accurately Timestamp Each Sensor Separately, Must Copy to Each Enumeration of Try/Except
                 times = ("{:04d}-{:02d}-{:02d} " "{:02d}:{:02d}:{:02d}").format(now[0], now[1], now[2], now[3], now[4], now[5], now[6], now[7])
                 #Appends Timestamp
                 timestamps.append(times)
             except Exception as e:
-                #Appends Placeholder Bus 0 Sensor Data
-                sensorsBus0.append(sensor_name)
-                addrsBus0.append(addrs)
-                tempsBus0.append(0.0)
-                humiditiesBus0.append(0.0)
-                #Takes Current Timestamp
-                now = time.localtime() #To Accurately Timestamp Each Sensor Separately, Must Copy to Each Enumeration of Try/Except
-                times = ("{:04d}-{:02d}-{:02d} " "{:02d}:{:02d}:{:02d}").format(now[0], now[1], now[2], now[3], now[4], now[5], now[6], now[7])
-                #Appends Timestamp
-                timestamps.append(times)
-                
-    #Debug Data for Bus 1 Print
-        sensorsBus1 = [] #Lists Bus 1 Sensors Names for Print Statements
-        addrsBus1 = [] #Lists Bus 1 Sensors Addresses for Print Statements
-        tempsBus1 = [] #Lists Bus 1 Sensors Temperatures for Print Statements
-        humiditiesBus1 = [] #Lists Bus 1 Sensors Humidities for Print Statements
-        for i, (sensor_name, addrs, sensor) in enumerate(HDC302X_Sensors.bus1):
-            try:
-                #Grabs Bus 1 Sensor Data
-                temp = sensor.temperature
-                hum = sensor.relative_humidity
-                #Appends Bus 1 Sensor Data
-                sensorsBus1.append(sensor_name)
-                addrsBus1.append(addrs)
-                tempsBus1.append(temp)
-                humiditiesBus1.append(hum)
-                #Takes Current Timestamp
-                now = time.localtime() #To Accurately Timestamp Each Sensor Separately, Must Copy to Each Enumeration of Try/Except
-                times = ("{:04d}-{:02d}-{:02d} " "{:02d}:{:02d}:{:02d}").format(now[0], now[1], now[2], now[3], now[4], now[5], now[6], now[7])
-                #Appends Timestamp
-                timestamps.append(times)
-            except Exception as e:
-                #Appends Placeholder Bus 1 Sensor Data
-                sensorsBus1.append(sensor_name)
-                addrsBus1.append(addrs)
-                tempsBus1.append(0.0)
-                humiditiesBus1.append(0.0)
+                #Appends Placeholder Sensor Data
+                nameList.append(sensor_name)
+                addrsList.append(addrs)
+                tempsList.append(0.0)
+                humsList.append(0.0)
                 #Takes Current Timestamp
                 now = time.localtime() #To Accurately Timestamp Each Sensor Separately, Must Copy to Each Enumeration of Try/Except
                 times = ("{:04d}-{:02d}-{:02d} " "{:02d}:{:02d}:{:02d}").format(now[0], now[1], now[2], now[3], now[4], now[5], now[6], now[7])
                 #Appends Timestamp
                 timestamps.append(times)
         
-        #Debug Prints for Bus 0 & Bus 1
+        #Debug Prints for Bus 0 & Bus 1 (All Sensors on sensorList) 
         print(f"\n================================SENSOR LOGGER================================")
         print(f"---BUS 0---")
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(sensorsBus0[0], addrsBus0[0], tempsBus0[0], humiditiesBus0[0], timestamps[0])) #Bus 0 Sensor 1 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(sensorsBus0[1], addrsBus0[1], tempsBus0[1], humiditiesBus0[1], timestamps[1])) #Bus 0 Sensor 2 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(sensorsBus0[2], addrsBus0[2], tempsBus0[2], humiditiesBus0[2], timestamps[2])) #Bus 0 Sensor 3 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(sensorsBus0[3], addrsBus0[3], tempsBus0[3], humiditiesBus0[3], timestamps[3])) #Bus 0 Sensor 4 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(nameList[0], addrsList[0], tempsList[0], humsList[0], timestamps[0])) #Bus 0 Sensor 1 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(nameList[1], addrsList[1], tempsList[1], humsList[1], timestamps[1])) #Bus 0 Sensor 2 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(nameList[2], addrsList[2], tempsList[2], humsList[2], timestamps[2])) #Bus 0 Sensor 3 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(nameList[3], addrsList[3], tempsList[3], humsList[3], timestamps[3])) #Bus 0 Sensor 4 Log
         print(f"---BUS 1---")
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(sensorsBus1[0], addrsBus1[0], tempsBus1[0], humiditiesBus1[0], timestamps[4])) #Bus 1 Sensor 5 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(sensorsBus1[1], addrsBus1[1], tempsBus1[1], humiditiesBus1[1], timestamps[5])) #Bus 1 Sensor 6 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(sensorsBus1[2], addrsBus1[2], tempsBus1[2], humiditiesBus1[2], timestamps[6])) #Bus 1 Sensor 7 Log
-        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(sensorsBus1[3], addrsBus1[3], tempsBus1[3], humiditiesBus1[3], timestamps[7])) #Bus 1 Sensor 8 Log
-        time.sleep(0.25) #Delay in Seconds (Repeats the Debug Prints this Often)
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(nameList[4], addrsList[4], tempsList[4], humsList[4], timestamps[4])) #Bus 1 Sensor 5 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(nameList[5], addrsList[5], tempsList[5], humsList[5], timestamps[5])) #Bus 1 Sensor 6 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(nameList[6], addrsList[6], tempsList[6], humsList[6], timestamps[6])) #Bus 1 Sensor 7 Log
+        print("{} ({}): Temperature = {:.2f}°C  |  Humidity = {:.1f}%  |  {}".format(nameList[7], addrsList[7], tempsList[7], humsList[7], timestamps[7])) #Bus 1 Sensor 8 Log
+        utime.sleep(1.0) #Delay in Seconds (Repeats the Debug Prints this Often)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~END OF debug_readings() FUNCTION~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
